ed0362e2eaff6512879f8225ddd874f027546228
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-08-21 23:55:38 +0530

    made registration form and added toastify notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Navbar, Homepage, Register, Login} from './components';
 
 function App() {
   return (
     <Router> 
+      <ToastContainer />
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Homepage></Homepage>} />
--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './register.css';
 
 function Register() {
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [cpassword, setCpassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+    if(password !== cpassword) toast.error("Passwords doesn't match");
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <div onSubmit={handleSubmit} className="master">
+        <form>
+          <div className="heading">
+            <h1>Registration Form</h1>
+          </div>
+          <div>
+            <label htmlFor="fname">First Name</label><br />
+            <input type="text" name="fname" id="fname" onChange={(e) => setFname(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="lname">Last Name</label><br />
+            <input type="text" name="lname" id="lname" onChange={(e) => setLname(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="email">Email</label><br />
+            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label><br />
+            <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="cpassword">Confirm Password</label><br />
+            <input type="cpassword" name="cpassword" id="cpassword" onChange={(e) => setCpassword(e.target.value)}/>
+          </div>
+          <button className="submitbtn" type="submit">Submit</button>
+        </form>
+      </div>
+    </>
   )
 }
 
